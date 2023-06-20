8d669aa3f7dc032bba8f22bb010271379c2e1dc9
Engineermorris <engineermorris5@gmail.com>
2023-06-20 16:43:50 -0700

    function neted

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Writes the character 'c' to stdout.
+ * Mputhia ventures ltd
+ * Return: 0 (success)
+ */
+
+int main(void);
+
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet print all alphabet in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+void (print_alphabet(void))
+{
+	char letter;
+
+	for (letter = 'a'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+return (0);
+}

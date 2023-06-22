c71ea70994e9931eccf4dcec971296a96328a572
Engineermorris <engineermorris5@gmail.com>
2023-06-21 23:43:36 -0700

    5sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-	putchar(43);
-	return (1);
+	_putchar(43);
+	return (+1);
 	}
 	else if (n < 0)
 	{
-	putchar(45);
+	_putchar(45);
 	return (-1);
 	}
 	else
